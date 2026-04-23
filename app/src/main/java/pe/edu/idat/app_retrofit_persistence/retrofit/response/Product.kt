cbfd966788979f363471ce482bb5a2f6dd8372c8
FernandoRetuerto <retuertofernando145@gmail.com>
2026-04-23 11:16:01 -0500

    entrega final

---
+++ app/src/main/java/pe/edu/idat/app_retrofit_persistence/retrofit/response/Product.kt
@@ -0,0 +1,8 @@
+package pe.edu.idat.app_retrofit_persistence.retrofit.response
+
+data class Product(
+    val title: String,
+    val price: Double,
+    val category: String,
+    val thumbnail: String
+)
---
+++ app/src/main/java/pe/edu/idat/app_retrofit_persistence/retrofit/response/ProductResponse.kt
@@ -0,0 +1,3 @@
+package pe.edu.idat.app_retrofit_persistence.retrofit.response
+
+data class ProductResponse(var products: List<Product>)
